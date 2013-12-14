f20405f2d64f58c3c31fddf5d0963ea9001c91e8
Dimitri Kurashvili <dimakura@gmail.com>
2013-12-15 01:41:08 +0400

    number to string

--- ka.js
+++ ka.js
@@ -51,4 +51,63 @@ if (typeof window != 'undefined') {
   module.toKa = function(text) {
     return this.translate(text, this.GEO, this.KA);
   };
+
+  // -- tokenize strings
+
+  module.numberToString = function(num) {
+    if (num < 0) { return 'მინუს' + tokenizePositiveInt(-num); }
+    else { return tokenizePositiveInt(num); }
+  }
+
+  var PRE_20 = [
+    'ნული', 'ერთი', 'ორი', 'სამი', 'ოთხი', 'ხუთი',
+    'ექვსი', 'შვიდი', 'რვა', 'ცხრა', 'ათი',
+    'თერთმეტი', 'თორმეტი', 'ცამეტი', 'თოთხმეტი', 'თხუთმეტი',
+    'თექვსმეტი', 'ჩვიდმეტი', 'თვრამეტი', 'ცხრამეტი'
+  ];
+  var BASE_20 = [ '', 'ორმ', 'სამ', 'ოთხმ' ];
+  var BASE_100 = [ '', 'ორ', 'სამ', 'ოთხ', 'ხუთ', 'ექვს', 'შვიდ', 'რვა', 'ცხრა' ];
+
+  var tokenize0 = function(num, base, ary, s1, s2) {
+    var m = Math.floor(num / base);
+    var r = num % base;
+    if (r != 0) {
+      return ary[m - 1] + s1  + ' ' + tokenizePositiveInt(r);
+    } else {
+      return ary[m - 1] + s2;
+    }
+  };
+
+  var tokenize1 = function(num, base, s1, s2) {
+    if (num == base) { return s2; }
+    var m = Math.floor(num / base);
+    var r = num % base;
+    if (r == 0) {
+      return tokenizePositiveInt(m) + ' ' + s2;
+    } else if (m == 1) {
+      return s1 + ' ' + tokenizePositiveInt(r);
+    } else {
+      return tokenizePositiveInt(m) + ' ' + s1 + ' ' + tokenizePositiveInt(r);
+    }
+  };
+
+  var tokenizePositiveInt = function(num) {
+    if (num < 20) {
+      return PRE_20[num];
+    } else if (num < 100) {
+      return tokenize0(num, 20, BASE_20, 'ოცდა', 'ოცი');
+    } else if (num < 1000) {
+      return tokenize0(num, 100, BASE_100, 'ას', 'ასი');
+    } else if (num < 1000000) {
+      return tokenize1(num, 1000, 'ათას', 'ათასი');
+    } else if (num < 1000000000) {
+      return tokenize1(num, 1000000, 'მილიონ', 'მილიონი');
+    } else if (num < 1000000000000) {
+      return tokenize1(num, 1000000000, 'მილიარდ', 'მილიარდი');
+    } else if (num < 1000000000000000) {
+      return tokenize1(num, 1000000000000, 'ტრილიონ', 'ტრილიონი');
+    }
+    return num;
+  };
+
 })(typeof window != 'undefined' ? window.KA : exports);
--- test/mobile_test.js
+++ test/mobile_test.js
@@ -28,3 +28,37 @@ describe('Translate', function() {
     assert.equal(ka.toGeo('დიმიტრი'), 'ÃÉÌÉÔÒÉ');
   });
 });
+
+describe('Number to string', function() {
+  describe('1-20 -> string', function() {
+    assert.equal(ka.numberToString(1), 'ერთი');
+    assert.equal(ka.numberToString(11), 'თერთმეტი');
+    assert.equal(ka.numberToString(20), 'ოცი');
+    assert.equal(ka.numberToString(21), 'ოცდა ერთი');
+    assert.equal(ka.numberToString(25), 'ოცდა ხუთი');
+    assert.equal(ka.numberToString(33), 'ოცდა ცამეტი');
+    assert.equal(ka.numberToString(40), 'ორმოცი');
+    assert.equal(ka.numberToString(60), 'სამოცი');
+    assert.equal(ka.numberToString(66), 'სამოცდა ექვსი');
+    assert.equal(ka.numberToString(80), 'ოთხმოცი');
+    assert.equal(ka.numberToString(99), 'ოთხმოცდა ცხრამეტი');
+    assert.equal(ka.numberToString(100), 'ასი');
+    assert.equal(ka.numberToString(200), 'ორასი');
+    assert.equal(ka.numberToString(300), 'სამასი');
+    assert.equal(ka.numberToString(400), 'ოთხასი');
+    assert.equal(ka.numberToString(500), 'ხუთასი');
+    assert.equal(ka.numberToString(600), 'ექვსასი');
+    assert.equal(ka.numberToString(700), 'შვიდასი');
+    assert.equal(ka.numberToString(800), 'რვაასი');
+    assert.equal(ka.numberToString(900), 'ცხრაასი');
+    assert.equal(ka.numberToString(999), 'ცხრაას ოთხმოცდა ცხრამეტი');
+    assert.equal(ka.numberToString(1000), 'ათასი');
+    assert.equal(ka.numberToString(2000), 'ორი ათასი');
+    assert.equal(ka.numberToString(900000), 'ცხრაასი ათასი');
+    assert.equal(ka.numberToString(1001), 'ათას ერთი');
+    assert.equal(ka.numberToString(1051), 'ათას ორმოცდა თერთმეტი');
+    assert.equal(ka.numberToString(999001), 'ცხრაას ოთხმოცდა ცხრამეტი ათას ერთი');
+  });
+});
+
+//console.log(ka.numberToString(999,999,999,999,999));
