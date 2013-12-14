95e82e973fb293ae92395a481e3607e99f561ed3
Dimitri Kurashvili <dimakura@gmail.com>
2013-12-15 00:38:19 +0400

    text translation

--- ka.js
+++ ka.js
@@ -1,28 +1,54 @@
-
-exports.compactMobile = function(mobile) {
-  return mobile.match(/\d+/g).join('');
-};
-
-exports.isLocalNumber = function(mobile) {
-  return this.compactMobile(mobile).length == 9;
+if (typeof window != 'undefined') {
+  window.KA = {};
 }
 
-exports.isGlobalNumber = function(mobile) {
-  return this.compactMobile(mobile).length == 12;
-};
+(function(module) {
+  // -- mobile operations
 
-exports.isValidMobile = function(mobile) {
-  return this.isLocalNumber(mobile) || this.isGlobalNumber(mobile);
-};
+  module.compactMobile = function(mobile) {
+    return mobile.match(/\d+/g).join('');
+  };
 
-exports.formatMobile = function(mobile) {
-  if (this.isGlobalNumber(mobile)) {
-    var m = this.compactMobile(mobile);
-    return ['(+', m.substr(0, 3), ' ', m.substr(3, 3), ')', m.substr(6, 3), '-', m.substr(9, 3)].join('');
-  } else if (this.isLocalNumber(mobile)) {
-    var m = this.compactMobile(mobile);
-    return ['(', m.substr(0, 3), ')', m.substr(3, 3), '-', m.substr(6, 3)].join('');
-  } else {
-    return mobile;
+  module.isLocalNumber = function(mobile) {
+    return this.compactMobile(mobile).length == 9;
   }
-};
+
+  module.isGlobalNumber = function(mobile) {
+    return this.compactMobile(mobile).length == 12;
+  };
+
+  module.isValidMobile = function(mobile) {
+    return this.isLocalNumber(mobile) || this.isGlobalNumber(mobile);
+  };
+
+  module.formatMobile = function(mobile) {
+    if (this.isGlobalNumber(mobile)) {
+      var m = this.compactMobile(mobile);
+      return ['(+', m.substr(0, 3), ' ', m.substr(3, 3), ')', m.substr(6, 3), '-', m.substr(9, 3)].join('');
+    } else if (this.isLocalNumber(mobile)) {
+      var m = this.compactMobile(mobile);
+      return ['(', m.substr(0, 3), ')', m.substr(3, 3), '-', m.substr(6, 3)].join('');
+    } else {
+      return mobile;
+    }
+  };
+
+  // -- translate strings
+
+  module.GEO = 'ÀÁÂÃÄÅÆÈÉÊËÌÍÏÐÑÒÓÔÖ×ØÙÚÛÜÝÞßàáãä'
+  module.KA  = 'აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ'
+  module.LAT = 'abgdevzTiklmnopJrstufqRySCcZwWxjh'
+
+  module.translate = function(text, from, to) {
+    var regExp = new RegExp(from.split('').join('|'), 'g');
+    return text.replace(regExp, function(c) { return to[from.indexOf(c)]; });
+  };
+
+  module.toGeo = function(text) {
+    return this.translate(text, this.KA, this.GEO);
+  };
+
+  module.toKa = function(text) {
+    return this.translate(text, this.GEO, this.KA);
+  };
+})(typeof window != 'undefined' ? window.KA : exports);
--- test/mobile_test.js
+++ test/mobile_test.js
@@ -1,8 +1,7 @@
 var assert = require('assert');
 var ka = require('../ka');
 
-var assert = require("assert")
-describe('Array', function(){
+describe('Mobile', function(){
   describe('mobile operations', function() {
     it('compacting mobile', function() {
       assert.equal(ka.compactMobile('599422451'), '599422451');
@@ -20,5 +19,12 @@ describe('Array', function(){
       assert.equal(ka.formatMobile('599422451'), '(599)422-451')
       assert.equal(ka.formatMobile('(995599)422451'), '(+995 599)422-451')
     });
+  });
+});
+
+describe('Translate', function() {
+  describe('ka <-> geo', function() {
+    assert.equal(ka.toKa(ka.toGeo('დიმიტრი')), 'დიმიტრი');
+    assert.equal(ka.toGeo('დიმიტრი'), 'ÃÉÌÉÔÒÉ');
   });
 });
